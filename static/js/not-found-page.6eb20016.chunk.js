88e024797aa77c515b7f09002c03058ba7adf9a8
Alina Kasianova <avkasyanova@gmail.com>
2021-11-06 18:32:52 +0200

    Updates

---
+++ static/js/not-found-page.6eb20016.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[6],{102:function(t,i,e){"use strict";e.r(i),e.d(i,"default",(function(){return r}));var c=e.p+"static/media/default_error.6ddcc1dc.png",n=e(1);function r(){return Object(n.jsx)("img",{style:{margin:"0 auto",width:"100%"},src:c,alt:"Nothing found"})}}}]);
+//# sourceMappingURL=not-found-page.6eb20016.chunk.js.map
